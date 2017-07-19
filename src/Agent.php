d6f05b291b6b77a1e78a01ec479b7b1044158019
chenghao <616915054@qq.com>
2017-07-19 17:59:39 +0800

    coding

--- src/Agent.php
+++ src/Agent.php
@@ -8,30 +8,55 @@ class Agent
     protected $client = NULL;
     protected static $readBufferLen = 8192;
 
-
     public function __construct(array $config)
     {
         self::checkEnv();
         $this->agentIp = $config['ip'];
         $this->agentPort = $config['port'];
-        $socketString = "udp://{$this->agentIp}:{$this->agentPort}";
-        $errno = $errstr = null;
-        $this->client = @stream_socket_client($socketString, $errno, $errstr);
-        if (!$this->client) {
-            exit("代理对象连接DNS服务器失败");
+        try{
+            $this->client = new Swoole\Client(SWOOLE_SOCK_UDP);
+            if (!$this->client->connect($this->agentIp, $this->agentPort,1)){
+                throw new Exception("error: connect dns server fail!", 1);
+            }
+        }catch (\Exception $e){
+            print $e;
         }
-        stream_set_blocking ($this->client,0);
     }
 
     public function send($data)
     {
-        return fwrite($this->client, $data);
+        return $this->client->send($data);
     }
+
 
     public function recv()
     {
-        return fread($this->client, static::$readBufferLen);
+        return $this->client->recv();
     }
+
+//    public function __construct(array $config)
+//    {
+//        self::checkEnv();
+//        $this->agentIp = $config['ip'];
+//        $this->agentPort = $config['port'];
+//        $socketString = "udp://{$this->agentIp}:{$this->agentPort}";
+//        $errno = $errstr = null;
+//        $this->client = @stream_socket_client($socketString, $errno, $errstr);
+//        if (!$this->client) {
+//            exit("代理对象连接DNS服务器失败");
+//        }
+//        stream_set_blocking ($this->client,0);
+//    }
+//
+//    public function send($data)
+//    {
+//        return fwrite($this->client, $data);
+//    }
+//
+//    public function recv()
+//    {
+//        return fread($this->client, static::$readBufferLen);
+//    }
 
     public static function checkEnv()
     {
--- src/Server.php
+++ src/Server.php
@@ -18,50 +18,82 @@ class Server
     {
         self::checkEnv();
         $this->listenIp = $config['ip'];
-        $this->listenPort = $config['port'];
+        $this->listenPort = (int)$config['port'];
         $this->agent = $agent;
-        // //创建dnsServer对象，类型为SWOOLE_SOCK_UDP
-        // $this->server = new Swoole\Server($this->listenIp, $this->listenPort, SWOOLE_BASE, SWOOLE_SOCK_UDP);
     }
-
 
     public function listen()
     {
-        $socketString = "udp://{$this->listenIp}:{$this->listenPort}";
-        $errno = $errstr = null;
-        $socket = stream_socket_server($socketString, $errno, $errstr, STREAM_SERVER_BIND);
-        do {
-            echo "start:------",PHP_EOL;
-            $request = stream_socket_recvfrom($socket, static::$recvBufferLen, 0, $address);
-            if (empty($request) || empty($address)) {
-                continue;
-            }
-            echo "IP:{$address}\nrequest:{$request}------",PHP_EOL;
-            $this->agent->send($request);
-            $response = $this->agent->recv();
-            echo "response:{$response}------",PHP_EOL;
-            $sendLen = stream_socket_sendto($socket, $response, 0, $address);
-            echo "send:{$sendLen}------IP:{$address}",PHP_EOL,PHP_EOL,PHP_EOL;
-        } while (true);
+        try{
+            $this->server = new Swoole\Server($this->listenIp, $this->listenPort, SWOOLE_BASE, SWOOLE_SOCK_UDP);
+            $this->server->on('Packet',[$this,'recv']);
+            $this->server->start();
+        }catch (\Exception $e){
 
+        }
     }
 
-    /**
-     * 接收请求处理方法
-     */
     public function recv($serv, $data, $clientInfo)
     {
-        // 调用请求前插件
-        plugin::begin($data,$clientInfo);
-        // 转发dns请求数据包给代理对象
-        $this->agent->send($data);
-        // 从agent对象获取结果
-        $responseDns = $this->agent->recv();
-        // 调用请求后插件
-        plugin::end($responseDns,$data,$clientInfo);
-        // 得到的结果回发给客户
-        $serv->sendto($clientInfo['address'], $clientInfo['port'], $responseDns);
+        if ($this->agent->send($data) > 0){
+            $msg = $this->agent->recv();
+            if ($msg){
+                $this->send($clientInfo, $msg);
+            }
+        }
+
     }
+
+    public function send($clientInfo,$data)
+    {
+        $remoteIp = $clientInfo['address'];
+        $port = $clientInfo['port'];
+        $this->server->sendto($remoteIp, $port, $data);
+    }
+
+
+
+
+
+
+
+//    public function listen()
+//    {
+//        $socketString = "udp://{$this->listenIp}:{$this->listenPort}";
+//        $errno = $errstr = null;
+//        $socket = stream_socket_server($socketString, $errno, $errstr, STREAM_SERVER_BIND);
+//        do {
+//            echo "start:------",PHP_EOL;
+//            $request = stream_socket_recvfrom($socket, static::$recvBufferLen, 0, $address);
+//            if (empty($request) || empty($address)) {
+//                continue;
+//            }
+//            echo "IP:{$address}\nrequest:{$request}------",PHP_EOL;
+//            $this->agent->send($request);
+//            $response = $this->agent->recv();
+//            echo "response:{$response}------",PHP_EOL;
+//            $sendLen = stream_socket_sendto($socket, $response, 0, $address);
+//            echo "send:{$sendLen}------IP:{$address}",PHP_EOL,PHP_EOL,PHP_EOL;
+//        } while (true);
+//
+//    }
+//
+//    /**
+//     * 接收请求处理方法
+//     */
+//    public function recv($serv, $data, $clientInfo)
+//    {
+//        // 调用请求前插件
+//        plugin::begin($data,$clientInfo);
+//        // 转发dns请求数据包给代理对象
+//        $this->agent->send($data);
+//        // 从agent对象获取结果
+//        $responseDns = $this->agent->recv();
+//        // 调用请求后插件
+//        plugin::end($responseDns,$data,$clientInfo);
+//        // 得到的结果回发给客户
+//        $serv->sendto($clientInfo['address'], $clientInfo['port'], $responseDns);
+//    }
 
     public static function checkEnv()
     {
